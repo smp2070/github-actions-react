0412260487842d1ad911dde58a128f8067fed8e0
roman_shvets <roman.shvets@easypark.net>
2024-01-12 13:41:23 +0200

    changed tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import { expect, test} from '@jest/globals';
+import myFunction from './myFunc';
 
-// test
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('adds 1 + 2 to equal 3', () => {
+  expect(myFunction(1, 2)).toBe(3);
 });
